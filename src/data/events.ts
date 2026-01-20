5a625131331f3fee1ff2d770be102d3b3352efb4
Maxime Musin <hello@maximemusin.me>
2026-01-20 20:07:43 +0100

    Update registration count for Spa May event from 8 to 10

--- src/data/events.ts
+++ src/data/events.ts
@@ -272,7 +272,7 @@ export const events: EventItem[] = [
     country: 'belgium',
     soldOut: false,
     capacity: 17,
-    registrations: 8,
+    registrations: 10,
     sessionPriceVatIncl: 240,
     sessionTime: '20 min',
     braceletPriceVatIncl: 60.5,
