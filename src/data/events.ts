dbfbfbce7bcd7af83988ce53713b5b753bd53aad
Maxime Musin <hello@maximemusin.me>
2026-02-02 09:18:52 +0100

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -257,7 +257,7 @@ export const events: EventItem[] = [
     country: 'belgium',
     soldOut: false,
     capacity: 17,
-    registrations: 16,
+    registrations: 17,
     sessionPriceVatIncl: 240,
     sessionTime: '20 min',
     braceletPriceVatIncl: 60.5,
